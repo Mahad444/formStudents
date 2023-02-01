8cec75d45f882c14a3ca249c2dc8f66e046388ac
Mahad444 <mahadsaid95@icloud.com>
2023-02-01 23:36:26 +0300

    form ofb registering students

---
+++ crud.php
@@ -0,0 +1,68 @@
+<?php
+include('dbh.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<?php
+if (isset($_POST["delete_student"])){
+    $student_id = $_POST['delete_student'];
+    
+
+    try{
+        $query = "DELETE FROM samplee WHERE Id=:stud_id";
+        $statement = $databaseConnection->prepare($query);
+        $data =[
+            ':stud_id' => $student_id
+            
+        ];
+        $statement->execute($data);
+
+        $query_execute = $statement->execute($data);
+        if ($query_execute){
+            header('location: students.php');
+        }else{
+            
+        }
+    }catch (PDOException $err){
+        echo $err->getMessage();
+    }
+}
+
+if (isset($_POST["update_student"])){
+    $student_id = htmlspecialchars($_POST['student_id']);
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $email = $_POST['email'];
+    $course = $_POST['course'];
+
+    try{
+        $query = "UPDATE samplee SET firstname=:firstname, lastname=:lastname, email=:email, course=:course WHERE Id=:stud_id";
+        $statement = $databaseConnection->prepare($query);
+        $data =[
+            ':firstname' => $firstname,
+            ':lastname' => $lastname,
+            ':email' => $email,
+            ':course' => $course,
+            ':stud_id' => $student_id
+        ];
+        $query_execute = $statement->execute($data);
+        if ($query_execute){
+            header('location: students.php');
+        }else{
+            //echo '<script> alert("Data NOT added")</script>';
+        }
+    }catch (PDOException $err){
+        echo $err->getMessage();
+    }
+}
+?>
+<body>
+    
+</body>
+</html>
